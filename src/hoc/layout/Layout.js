4c61ee807af0dd98f072566f8735ca6e706412bf
horaciocoronel <arielc@gmail.com>
2018-04-27 12:52:15 -0400

    Pass props to Header

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -5,12 +5,19 @@ import Header from '../../components/header/Header';
 
 class Layout extends Component {
     state = {
-
+      showNav: false
     }
+
+    toggleSideNav = (action) => { this.setState({showNav: action}) }
+    
     render(){
         return (
             <div>
-                <Header />
+                <Header 
+                  showNav={this.state.showNav}
+                  onHideNav={() =>  this.toggleSideNav(false)}
+                  onOpenNav={() =>  this.toggleSideNav(true)}
+                />
                 {this.props.children}
             </div>
         )    
