91819cded295c7dc484ba95d96ed54809107d851
horaciocoronel <arielc@gmail.com>
2018-04-30 02:33:22 -0400

    Add TeamInfo and PostData subcomponents to display data

--- src/components/articles/news/post/Header.js
+++ src/components/articles/news/post/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import TeamInfo from '../../elements/TeamInfo';
+import PostData from '../../elements/PostData';
 
 const Header = (props) => {
+
+  const teamInfo = (team) => {
+    return team ? (
+      <TeamInfo team={team}/>
+    ) : null
+  }
+
+  const postData = (date, author) => (
+    <PostData data={{date, author}} />
+  )
+
   return (
     <div>
-      Header
+      {teamInfo(props.teamData)}
+      {postData(props.articleDate, props.author)}
     </div>
   )
 }
