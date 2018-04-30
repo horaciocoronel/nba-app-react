319b813b870ac9b2f8969e441097759cef2d2333
horaciocoronel <arielc@gmail.com>
2018-04-30 00:30:16 -0400

    Add VideoList and VideoListTemplate components plus styling

--- src/components/widgets/videosList/VideosList.js
+++ src/components/widgets/videosList/VideosList.js
@@ -3,6 +3,7 @@ import styles from './videosList.css';
 import axios from 'axios';
 import { URL } from '../../../config';
 import Button from '../buttons/Buttons';
+import VideosListTemplate from './VideosListTemplate';
 
 
 export class VideosList extends Component {
@@ -14,11 +15,41 @@ export class VideosList extends Component {
     amount: this.props.amount
   }
 
+  componentWillMount() {
+    this.request(this.state.start, this.state.end)
+  }
+
   renderTitle = () => {
     return this.props.title ? (
       <h3><strong>NBA</strong> Videos</h3>
     ) : null
   }
+
+  request = (start, end) => {
+    if(this.state.teams.length < 1 ) {
+      axios.get(`${URL}/teams`)
+        .then(res => {
+          this.setState({ teams: res.data })
+        })
+    }
+    axios.get(`${URL}/videos?_start=${start}&_end=${end}`)
+      .then(res => {
+        this.setState({ videos: [...this.state.videos, ...res.data]})
+      })
+  }
+
+  renderVideos = () => {
+    let template = null;
+    switch(this.props.type) {
+      case('card'):
+        template = <VideosListTemplate data={this.state.videos} teams={this.state.teams} />
+        break;
+      default:
+        template = null;
+    }
+    return template;
+  }
+  
 
   loadMore = () => {
     
@@ -45,6 +76,7 @@ export class VideosList extends Component {
     return (
       <div className={styles.videoList_wrapper}>
         {this.renderTitle()}
+        {this.renderVideos()}
         {this.renderButton()}
       </div>
     )
