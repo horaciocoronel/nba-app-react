0ae212bac03b99dda7300bf42c160535e1ef94c6
horaciocoronel <arielc@gmail.com>
2018-04-30 02:50:03 -0400

    Add VideoArticles subcomponents and display data

---
+++ src/components/articles/videos/video/Index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { URL } from '../../../../config';
+
+import styles from '../../articles.css';
+import Header from './Header';
+
+export class VideoArticle extends Component {
+
+  state = {
+    article: [],
+    team: []
+  }
+
+  componentWillMount() {
+    axios.get(`${URL}/videos?id=${this.props.match.params.id}`)
+    .then(res => {
+      let article = res.data[0];
+      axios.get(`${URL}/teams?id=${article.team}`)
+        .then(res => {
+          this.setState({
+            article,
+            team: res.data
+          })
+        })
+    })
+  }
+  render() {
+    const article = this.state.article;
+    const team = this.state.team;
+    return (
+      <div>
+        <Header teamData={team[0]} />
+        VideoArticle
+      </div>
+    )
+  }
+}
+
+export default VideoArticle
