f73d45bbaccecd972ae1c50a93b6bced0fc32f1c
horaciocoronel <arielc@gmail.com>
2018-05-01 00:50:00 -0400

    Add /news route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/Home';
 import Layout from './hoc/layout/Layout';
 import NewsArticle from './components/articles/news/post/Index';
+import NewsMain from './components/articles/news/main/NewsMain';
 import VideoArticle from './components/articles/videos/video/Index';
 
 class Routes extends Component {
@@ -13,6 +14,7 @@ class Routes extends Component {
                     <Route path="/" exact component={Home} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
                     <Route path="/videos/:id" exact component={VideoArticle} />
+                    <Route path="/news" exact component={NewsMain} />
                 </Switch>
             </Layout>
         );
