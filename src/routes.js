0270089c161b64477157f3ae54e76e71819bf4c7
horaciocoronel <arielc@gmail.com>
2018-05-01 01:01:18 -0400

    Add /videos route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import Layout from './hoc/layout/Layout';
 import NewsArticle from './components/articles/news/post/Index';
 import NewsMain from './components/articles/news/main/NewsMain';
+import VideosMain from './components/articles/news/main/VideosMain';
 import VideoArticle from './components/articles/videos/video/Index';
 
 class Routes extends Component {
@@ -12,9 +13,10 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home} />
+                    <Route path="/news" exact component={NewsMain} />
+                    <Route path="/videos" exact component={VideosMain} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
                     <Route path="/videos/:id" exact component={VideoArticle} />
-                    <Route path="/news" exact component={NewsMain} />
                 </Switch>
             </Layout>
         );
